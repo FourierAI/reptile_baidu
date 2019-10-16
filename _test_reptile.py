98318b642880bb28ab52c3008f6e75b17855f490
MrYezhipeng <939647181@qq.com>
2019-10-16 11:04:37 +0800

    refactor code

--- _test_reptile.py
+++ _test_reptile.py
@@ -1,87 +1,135 @@
 #!/usr/bin/python
 
 import requests
+from requests import ReadTimeout
 from bs4 import BeautifulSoup
 
-keywords = []
-file_keyword = open('/users/geekye/Desktop/keywords.txt')
 
-try:
-    file_content = file_keyword.read()
-    keywords = file_content.splitlines()
-finally:
-    file_keyword.close()
+def generate_request_baidu_url(file_path):
+    file_keyword = open(file_path)
+    try:
+        file_content = file_keyword.read()
+        keywords = file_content.splitlines()
+    finally:
+        file_keyword.close()
+    urls = []
+    for keyword in keywords:
+        # failure to reptile information from bing
+        # url = "https://cn.bing.com/search?q=" + keyword
+        # url = "https://bing.com/search?q=" + keyword + "&PC=U" + str(pc_code) + "&FORM=CHROMN"
+        # it seem that baidu is ok
+        url = "https://www.baidu.com/s?ie=utf-8&wd=" + keyword
+        urls.append(url)
+    # imitate as browser
+    request_hearders = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+    }
 
-urls = []
+    return urls, request_hearders
 
-for keyword in keywords:
-    # failure to reptile information from bing
-    # url = "https://cn.bing.com/search?q=" + keyword
-    # url = "https://bing.com/search?q=" + keyword + "&PC=U" + str(pc_code) + "&FORM=CHROMN"
-    # it seem that baidu is ok
-    url = "https://www.baidu.com/s?ie=utf-8&wd=" + keyword
-    urls.append(url)
 
-# imitate as browser
-request_hearders = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
-}
+def get_urls_in_baidu(urls, request_hearders):
+    removed_invalid_url = []
+    # get information from baidu
+    for i in range(len(urls)):
 
-# valid url list
-removed_invalid_url = []
+        try:
+            # set timeout and turnoff ssl
+            r = requests.get(urls[i], headers=request_hearders, timeout=2)
+            if r.status_code == 200:
 
-# get information from baidu
-for i in range(len(urls)):
+                html_doc = r.content[2:]
 
-    # set timeout and turnoff ssl
-    r = requests.get(urls[i], headers=request_hearders, timeout=0.8, verify=True)
+                soup = BeautifulSoup(html_doc, 'html.parser')
 
-    if r.status_code == 200:
+                html_content = soup.get_text
 
-        html_doc = r.content[2:]
+                b_content = soup.find('div', id='content_left')
 
-        soup = BeautifulSoup(html_doc, 'html.parser')
+                list1 = b_content.find_all('a', href=True)
 
-        html_content = soup.get_text;
+                for elem in list1:
+                    url = elem['href']
+                    if url.startswith('https') or url.startswith('http'):
+                        removed_invalid_url.append(url)
 
-        b_content = soup.find('div', id='content_left')
+        except (requests.exceptions.ConnectionError, ReadTimeout):
+            print('get Baidu information timeout!')
+            continue
 
-        list1 = b_content.find_all('a', href=True)
+    return removed_invalid_url
 
-        for elem in list1:
-            url = elem['href']
-            if url.startswith('https') or url.startswith('http'):
-                removed_invalid_url.append(url)
 
-if len(removed_invalid_url) < 1:
-    raise Exception('there is nothing in removed_invalid_url')
+def separater_direct_indirect_list(removed_invalid_url):
+    # remove same elements
+    removed_invalid_url_set = set(removed_invalid_url)
+    url_direct_list = []
+    url_redirect_list = []
+    for url in removed_invalid_url:
+        if "link?url=" in url:
+            url_redirect_list.append(url)
+        else:
+            url_direct_list.append(url)
+    return url_direct_list, url_redirect_list
 
-# remove same elements
-removed_invalid_url_set = set(removed_invalid_url)
 
-url_direct_list = []
-url_redirect_list = []
+def generate_html_list(url_direct_list, url_redirect_list):
+    html_list = []
 
-for url in removed_invalid_url:
-    if "link?url=" in url:
-        url_redirect_list.append(url)
-    else:
-        url_direct_list.append(url)
+    # opearte respectively
+    for url in url_direct_list:
+        try:
+            response = requests.get(url, headers=request_hearders, timeout=2)
+        except (requests.exceptions.ConnectionError, ReadTimeout):
+            print("Direct connection is timeout!")
+            continue
+        html_list.append(response.content)
 
-html_list = []
+    for url in url_redirect_list:
 
-# opearte respectively
-for url in url_direct_list:
-    response = requests.get(url, headers=request_hearders, timeout=0.8, verify=True)
-    html_list.append(response.content)
+        # turn off redirect function
+        try:
+            res = requests.get(url=url, allow_redirects=False, headers=request_hearders, timeout=2)
+        except (requests.exceptions.ConnectionError, ReadTimeout):
+            print("Redirect is failure!")
+            continue
 
-for url in url_redirect_list:
-    # turn off redirect function
-    res = requests.get(url=url, allow_redirects=False, headers=request_hearders, timeout=0.8, verify=True)
-    direct_url = res.headers['location']
+        if (res.status_code == 302):
+            direct_url = res.headers['location']
+        else:
+            continue
 
-    response = requests.get(direct_url, allow_redirects=False, headers=request_hearders, timeout=0.8, verify=True)
-    html_list.append(response.content)
+        try:
+            response = requests.get(direct_url, allow_redirects=False, headers=request_hearders, timeout=2)
+        except (requests.exceptions.ConnectionError, ReadTimeout):
+            print("Reptile original website is failure!")
+            continue
 
-print(len(html_list))
-print('hello html')
+        if (res.status_code == 200):
+            html_list.append(response.content)
+
+    return html_list
+
+
+# main function
+if __name__ == '__main__':
+
+    file_path = '/users/geekye/Desktop/keywords.txt'
+    # generate urls of request of baidu from txt file
+    urls, request_hearders = generate_request_baidu_url(file_path)
+
+    removed_invalid_url = get_urls_in_baidu(urls, request_hearders)
+
+    if len(removed_invalid_url) < 1:
+        raise Exception('there is nothing in removed_invalid_url')
+
+    # separater_direct_indirect_list
+    url_direct_list, url_redirect_list = separater_direct_indirect_list(removed_invalid_url)
+
+    html_list = generate_html_list(url_direct_list, url_redirect_list)
+
+    # test
+    for html in html_list:
+        print(html)
+
+    print("the length of html list:" + str(len(html_list)))
